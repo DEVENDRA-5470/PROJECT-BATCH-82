9c7a2f98dfab19fdd4e177f010819b2d2baeaf41
DEVENDRA-5470 <ysdevn@gmail.com>
2025-09-19 12:13:26 +0000

    auto backup Fri Sep 19 12:13:26 UTC 2025

--- Flask/third-project/app.py
+++ Flask/third-project/app.py
@@ -61,6 +61,41 @@ def delete_stu(id):
     except Exception as e:
          print(e)
     return f"Data not deleted"
+
+@app.route("/edit/<int:id>",methods=['GET','POST'])
+def edit_user(id):
+    if request.method=="POST":
+          conn=get_connection(DB_NAME)
+          full_name=request.form.get('full-name')
+          email=request.form.get('email')
+          password=request.form.get('password')
+          print(full_name,email,password)
+          try:
+                with conn.cursor() as cursor:
+                      query="UPDATE STUDENTS SET SFULL_NAME=%s,SEMAIL=%s ,SPASS=%s WHERE SID=%s"
+                      values=(full_name,email,password,id)
+                      cursor.execute(query,values)
+                      conn.commit()
+                      return redirect(url_for('get_all_stu'))
+          except Exception as e:
+                print(e)
+          return f"Data Not updated.. {e}"
+    else:
+        user=[]
+        conn=get_connection(DB_NAME)
+        try:
+            with conn.cursor() as cursor:
+                    query=f'''SELECT * FROM STUDENTS WHERE SID={id}'''
+                    cursor.execute(query)
+                    for i in cursor.fetchall():
+                        user.append(i)
+                    print(user)
+                    
+        except Exception as e:
+                print("Error :",e)
+        finally:
+                conn.close()
+        return render_template("edit_stu.html",users=user)
       
 
      
